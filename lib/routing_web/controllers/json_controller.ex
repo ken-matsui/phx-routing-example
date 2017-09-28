59be0d955b1104ac421f1fc49c19609ed924edcf
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2017-09-28 19:50:19 +0900

    modify controllers/json_controller.ex

--- lib/routing_web/controllers/json_controller.ex
+++ lib/routing_web/controllers/json_controller.ex
@@ -1,18 +1,12 @@
 defmodule RoutingWeb.JsonController do
 	use RoutingWeb, :controller
 
-	# def index(conn, _params) do
-	# 	# 初期がこれ
-	# 	#render conn, "index.html"
-	# 	# flash messageの生成が以下
-	# 	conn
-	# 	|> put_flash(:error, "Please put something behind hello/.")
-	# 	|> render("index.html")
-	# end
+	def index(conn, _params) do
+		text conn, "Plain text."
+	end
 
 	def show(conn, %{"id" => id}) do
 		# renderの第三引数はViewに渡される．
-		#render conn, "show.html", messenger: messenger
 		json conn, %{id: id}
 	end
 end
