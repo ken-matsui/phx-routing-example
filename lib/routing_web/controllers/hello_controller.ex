e313d533253b01f973aeda3b3525a1548bf6a343
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2017-09-28 19:21:15 +0900

    modify controllers/hello_controller.ex

--- lib/routing_web/controllers/hello_controller.ex
+++ lib/routing_web/controllers/hello_controller.ex
@@ -3,16 +3,18 @@ defmodule RoutingWeb.HelloController do
 
 	def index(conn, _params) do
 		# 初期がこれ
-		# render conn, "index.html"
+		#render conn, "index.html"
 		# flash messageの生成が以下
 		conn
-		|> put_flash(:info, "Welcome to Phoenix, from flash info!")
-		|> put_flash(:error, "Let's, pretend we have an error.")
+		|> put_flash(:error, "Please put something behind hello/.")
 		|> render("index.html")
 	end
 
 	def show(conn, %{"messenger" => messenger}) do
 		# renderの第三引数はViewに渡される．
-		render conn, "show.html", messenger: messenger
+		#render conn, "show.html", messenger: messenger
+		conn
+		|> put_flash(:info, "Welcome to Phoenix, from flash info!")
+		|> render("show.html", messenger: messenger)
 	end
 end
