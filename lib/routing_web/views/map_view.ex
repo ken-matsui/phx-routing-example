eec541a1331948990cd7295ffd83b4d88193e5d2
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2017-09-28 23:58:02 +0900

    modify map_view.ex

--- lib/routing_web/views/map_view.ex
+++ lib/routing_web/views/map_view.ex
@@ -1,6 +1,10 @@
 defmodule RoutingWeb.MapView do
   use RoutingWeb, :view
 
+  def handler_info(conn) do
+    "Request Handled By: #{controller_module conn}.#{action_name conn}"
+  end
+
   def connection_keys(conn) do
     conn
     |> Map.from_struct()
